d1d966953180fd1aa4ce21eaead47b0f4d4f2790
chenzhongtian <chenzhongtian2010@qq.com>
2020-06-05 09:18:14 +0800

    修改方式

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,30 +1,63 @@
-﻿using System;
+﻿using ConsoleApp1.Models;
+using SqlSugar;
 using System.Collections.Generic;
-using System.Linq;
-using ConsoleApp.Data;
-using ConsoleApp.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            using (TestContext db=new TestContext())
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
             {
-                List<Gddru> list = db.Gddru.ToList();
-                foreach (var item in list)
+                DbType = DbType.SqlServer,
+                ConnectionString = "Data Source=.;Initial Catalog=Test;Integrated Security=True",
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute
+            });
+
+            List<GDDRU> gDDRUs = db.Queryable<GDDRU>().ToList();
+            List<GDFAN> gDFANs = db.Queryable<GDFAN>().ToList();
+
+
+            List<string> tableNameList = new List<string> { "fmu", "med", "chk", "dru","lab","ope" };
+
+
+            foreach (var gDFANItem in gDFANs)
+            {
+                if (tableNameList.Contains(gDFANItem.FTBL.ToLower()))
                 {
-                    string sql = $"select * from {item.Drutbl} where conid={item.Druconid} and cvocable='{item.Druname}'";
-                    var l=db.Gddru.FromSqlRaw(sql).ToList();
-                    foreach (var litem in l)
+                    string sql = $"select * from {gDFANItem.FTBL} where conid={gDFANItem.FCONID} and cvocable='{gDFANItem.FSTR}'";
+
+                    var list = db.SqlQueryable<dynamic>(sql).ToList();
+                    foreach (var item in list)
                     {
-                        Console.WriteLine();
+                        //CVOCABLE  CONID
+                        System.Console.WriteLine(item.CVOCABLE.ToString());
+                        gDFANItem.FKEY_NO = item.KEY_NO;
                     }
+                    db.Updateable(gDFANItem).WhereColumns(e => new { e.FCONID, e.FSTR, e.id, e.IFID }).ExecuteCommand();
                 }
             }
-          
+
+
+
+            //foreach (var gDDRUItem in gDDRUs)
+            //{
+            //    if (tableNameList.Contains(gDDRUItem.DRUTBL.ToLower()))
+            //    {
+            //        string sql = $"select * from {gDDRUItem.DRUTBL} where conid={gDDRUItem.DRUCONID} and cvocable='{gDDRUItem.DRUNAME}'";
+
+            //        var list = db.SqlQueryable<dynamic>(sql).ToList();
+            //        foreach (var item in list)
+            //        {
+            //            //CVOCABLE  CONID
+            //            System.Console.WriteLine(item.CVOCABLE.ToString());
+            //            gDDRUItem.DRUKey_No = item.KEY_NO;
+            //        }
+            //        db.Updateable(gDDRUItem).WhereColumns(e=>new {e.DRUCONID,e.DRUNAME,e.id,e.IFID }).ExecuteCommand();
+            //    }
+            //}
         }
     }
 }
